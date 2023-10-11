689c62b34249a1e44d12fef82249e20846242b82
Lino004 <h.ayinlamouhayad@gmail.com>
2023-10-11 01:53:23 +0000

    Removed all files that are in the .gitignore

--- dist/services/controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendResponse = void 0;
-function sendResponse(val) {
-    return { data: val };
-}
-exports.sendResponse = sendResponse;
-//# sourceMappingURL=controller.js.map
